3e7e0c3a7a34cdb6f80d5ea745eec51bc9919a57
Faith <faithbrianna01@gmail.com>
2023-12-16 11:15:45 +0300

    error message

--- studentgenerator.js
+++ studentgenerator.js
@@ -1,6 +1,4 @@
-//Write a function that prompts the user to input student marks. The input should be between 0 and 100. The output should correspond the correct grade, as shown below:
 
-//A > 79, B - 60 to 79, C -  59 to 49, D - 40 to 49, E - less 40.
 //Prompt-sync to get user input
 const prompt = require('prompt-sync')();
 //Parsefloat to convert user input to float
